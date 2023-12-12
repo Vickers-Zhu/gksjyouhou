439f35a63ec0f38d62a093012a6139f120612ce0
Vickers Zhu <vickerszhu@163.com>
2023-12-13 03:51:38 +0900

    prompt improved

--- rag.py
+++ rag.py
@@ -22,23 +22,17 @@ class Retriever:
             query_texts=[input],
             n_results=k,
         )
-        print(retrieval_results)
+        print(retrieval_results["metadatas"][0])
         print('-----------------')
         return retrieval_results["metadatas"][0]
 
 class Generator:
     def __init__(self, openai_model="gpt-3.5-turbo"):
         self.openai_model = openai_model
-        self.prompt_template_profile = """
-            {profile}
-        """
-        self.prompt_template_ques = """
-            You're a helpful assistant. 
-            Based on the profiles and user's question, 
-            provide a recommendation from the aboving candidate profiles in Japanese:
-
-            Question: {user_question}
-        """
+        self.prompt_template_profile = """{profile}"""
+        self.prompt_template_ques = """You're a helpful assistant. Based on the profiles and user's question, provide a recommendation from the aboving candidate profiles in Japanese:
+            Question: {user_question}"""
+        self.prompt_template_assistant = """"""
 
     def generate_response(self, retrieval_results, questions):
         prompts = []
@@ -46,14 +40,12 @@ class Generator:
             prompt = self.prompt_template_profile.format(profile=result.get('info'))
             prompts.append(prompt)
         prompts.reverse()
-        self.prompt_template_ques.format(user_question=questions)
-        prompts.append(self.prompt_template_ques)
-        print(prompts)
-
+        messages=[{"role": "system", "content": prompt} for prompt in prompts]
+        messages.append({"role": "user", "content": self.prompt_template_ques.format(user_question=questions)})
+        print(messages)
         response = openai.OpenAI().chat.completions.create(
             model=self.openai_model,
-            messages=[{"role": "assistant", "content": prompt} for prompt in prompts],
-            temperature=0,
+            messages=messages
         )
 
         return response.choices[0].message.content
@@ -66,11 +58,11 @@ class Chatbot:
     def answer(self, input):
         k = 5  # Number of profiles to retrieve
         retrieval_results = self.retriever.get_retrieval_results(input, k)
-        return self.generator.generate_response(retrieval_results, user_input)
+        return self.generator.generate_response(retrieval_results, input)
 
 # Creating an instance of the Chatbot class
 chatbot = Chatbot()
 
-user_input = input("You: ")
+user_input = input("User prompt: ")
 response = chatbot.answer(user_input)
 print(f"Chatbot: {response}")
